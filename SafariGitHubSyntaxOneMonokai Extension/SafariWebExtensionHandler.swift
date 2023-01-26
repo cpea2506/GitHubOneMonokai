bb445401e3dc772dde0c157a0d044993d2bd2a5a
CPea <vienpro2506@gmail.com>
2023-01-27 01:32:49 +0700

    rename to SafariGitHubSyntaxOneMonokai

---
+++ SafariGitHubSyntaxOneMonokai Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,26 @@
+//
+// Created by Do Vien on 27/01/2023 at 01:01.
+//
+// Copyright © 2023 CPea2506. All rights reserved.
+// 
+
+
+import SafariServices
+import os.log
+
+let SFExtensionMessageKey = "message"
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
