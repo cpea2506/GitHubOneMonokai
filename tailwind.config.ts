544c8259cad6b019188441a696380949f54d7e98
CPea <cpea2506@gmail.com>
2023-07-05 20:11:56 +0700

    convert to `scss`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,24 @@ import { Config } from "tailwindcss"
 const config: Config = {
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                fg: "#abb2bf",
+                black: "#282c34",
+                gray: "#676e7b",
+                pink: "#e06c75",
+                green: "#98c379",
+                cyan: "#56b6c2",
+                aqua: "#61afef",
+                yellow: "#e5c07b",
+                purple: "#c678dd",
+                orange: "#d19a66",
+                red: "#f75f5f",
+                lightGray: "#9ca3b2",
+                darkGray: "#4b5261",
+                white: "#d7d7ff",
+            }
+        },
     },
     plugins: [],
 }
